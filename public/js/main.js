2836b99619aaeb094a3dce22707beb52f10e5116
cichykuba63 <228085@student.uek.krakow.pl>
2025-05-05 11:34:17 +0200

    move script fragment

--- public/js/main.js
+++ public/js/main.js
@@ -32,6 +32,23 @@ function clearPeopleMarkers() {
 	}
 }
 
+// dystans pomiędzy dwoma punktami
+function calculateDistance(lat1, lon1, lat2, lon2) {
+	const R = 6371000 // promień Ziemi w metrach
+	const toRad = deg => deg * (Math.PI / 180)
+
+	const dLat = toRad(lat2 - lat1)
+	const dLon = toRad(lon2 - lon1)
+
+	const a =
+		Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+		Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2)
+
+	const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+	return R * c // odległość w metrach
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
 	// Usuwanie lokalizacji z poprzedniej sesji
 	const previousLocationId = localStorage.getItem("userLocationDocId")
@@ -273,3 +290,12 @@ document.addEventListener("DOMContentLoaded", async () => {
 })
 
 export { removeMarker, clearPeopleMarkers }
+
+if ("serviceWorker" in navigator) {
+	window.addEventListener("load", function () {
+		navigator.serviceWorker
+			.register("/serviceWorker.js")
+			.then(res => console.log("service worker registered"))
+			.catch(err => console.log("service worker not registered", err))
+	})
+}
